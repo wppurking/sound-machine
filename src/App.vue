20670e5955ba64e5301f430a3c9b939f4029afae
wyatt <wppurking@gmail.com>
2016-08-10 21:44:57 +0800

    * 将 beep-button 的布局弄好.

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,46 @@
       <img src="assets/img/speaker.png">
     </div>
     <div class="buttons">
-      这里全部是按钮
+      <template v-for='btn in buttons'>
+        <beep-button :beep='btn.beep'></beep-button>
+      </template>
     </div>
   </div>
 </template>
 
 <script>
   import 'assets/main.scss';
+  import BeepButton from 'vues/beep-button';
 
   export default {
-    components: {}
+    data() {
+      return {
+        buttons: [
+          {beep: 'ba-dum-tsss'},
+          {beep: 'ba-dum-tsss'},
+          {beep: 'ba-dum-tsss'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'},
+          {icon: 'applause', beep: 'applause'}
+        ]
+      }
+    },
+    components: {
+      BeepButton
+    }
   }
 </script>
---
+++ src/components/beep-button.vue
@@ -0,0 +1,20 @@
+// 有声音的按钮, 具体声音参数由外部传入
+<template>
+  <div class='beep-button'>
+    <span class='icon {{icon}}'></span>
+  </div>
+</template>
+<script>
+  export default{
+    props: {
+      icon: {
+        type: String,
+        default: 'ba-dum-tsss'
+      },
+      beep: {
+        type: String,
+        requried: true
+      }
+    }
+  }
+</script>
