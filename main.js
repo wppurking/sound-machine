e287cfe6741cb51558a560103ed0eacb32fb9a11
wyatt <wppurking@gmail.com>
2016-08-10 20:36:25 +0800

    * 使用 IDEA 进行格式化.

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ const { app, BrowserWindow } = require('electron');
 var mainWindow = null;
 
 app.on('ready', () => {
-	if(!mainWindow) {
-		mainWindow = new BrowserWindow({
-			width: 800,
-			height: 600
-		});
+  if(!mainWindow) {
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      resizable: false
+    });
 
-		mainWindow.loadURL('file://' + __dirname + "/index.html");
-		mainWindow.on('closed', () => {
-			mainWindow = null;
-		});
-	}
+    mainWindow.loadURL('file://' + __dirname + "/index.html");
+    mainWindow.on('closed', () => {
+      mainWindow = null;
+    });
+  }
 });
